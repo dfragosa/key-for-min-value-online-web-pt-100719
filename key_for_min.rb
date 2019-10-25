e2515051010709d78789d26a6196342642e7fec7
Derric Fragosa <fderric@gmail.com>
2019-10-25 07:33:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,10 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-
-#key  #keys  #values  #min  #sort  #sort_by  #min_by
-#ABOVE R OFF LIMITS
-
 def key_for_min_value(name_hash)
   low_value = nil
   low_key = nil 
